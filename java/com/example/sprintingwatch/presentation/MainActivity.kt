5a71e45ccadc3afcaa95ff430a9ffb30211dc5a5
3xTomorrow <denyszagrebin7@gmail.com>
2025-09-13 15:15:45 -0400

    Trying to implement Handler with the RSSI but there is a bug It looks like its getting the RSSI in the beginning, when the program starts then it keeps that value without updating, even though it inside a Handler loop

--- java/com/example/sprintingwatch/presentation/MainActivity.kt
+++ java/com/example/sprintingwatch/presentation/MainActivity.kt
@@ -2,17 +2,21 @@ package com.example.sprintingwatch.presentation
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.wifi.WifiInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.sprintingwatch.R
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.annotation.RequiresPermission
 import kotlinx.coroutines.Runnable
-
 
 enum class TimerStates {
     INITIAL,
@@ -23,7 +27,7 @@ enum class TimerStates {
 
 class MainActivity : ComponentActivity() {
 
-    @SuppressLint("SetTextI18n", "MissingInflatedId")
+    @SuppressLint("SetTextI18n", "MissingInflatedId", "ServiceCast")
     @RequiresPermission(Manifest.permission.ACCESS_NETWORK_STATE)
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
@@ -38,93 +42,104 @@ class MainActivity : ComponentActivity() {
         val pauseButton: Button = findViewById<Button>(R.id.pause_button)
         val rssiText: TextView = findViewById<TextView>(R.id.rssiText)
 
-//       startButton.setOnClickListener {
-//           runOnUiThread {
-//               var wifiInfo: WifiInfo? = null
-//
-//               val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-//               val activeNetwork = connectivityManager.activeNetwork
-//               val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
-//
-//               if (networkCapabilities != null && networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-//                   wifiInfo = networkCapabilities.transportInfo as? WifiInfo
-//               }
-//
-//               val rssi: Int = wifiInfo?.rssi ?: Log.d("ERROR", "Can't find RSSI")  // in dBm
-//               val frequency: Int = wifiInfo?.frequency ?: Log.d("ERROR", "Can't find Frequency")
-//               val ssid = wifiInfo?.ssid ?: Log.d("ERROR", "Can't find SSID")
-//               val linkSpeed: Int = wifiInfo?.linkSpeed ?: Log.d("ERROR", "Can't find Link Speed")
-//
-//
-//               //Logs to test wifi
-//               Log.d("WifiInfo", "RSSI: $rssi dBm")
-//               Log.d("WifiName", "SSID: $ssid, Frequency: $frequency MHz, Link Speed: $linkSpeed")
-//
-//
-//               rssiText.text = "$rssi dBm"
-//           }
-//       }
+        val handler = Handler(Looper.getMainLooper())
+        lateinit var runnable: Runnable
 
-        var states: TimerStates = TimerStates.INITIAL
-        val timer = HandlerTimer()
 
-        startButton.setOnClickListener {
-            if (states == TimerStates.INITIAL || states == TimerStates.CANCELLED) {
-                timer.start(rssiText)
-                states = TimerStates.RUNNING
-            } else if (states == TimerStates.PAUSED) {
-                timer.resume()
-                states = TimerStates.RUNNING
-            }
-        }
 
-        pauseButton.setOnClickListener {
-            if (states == TimerStates.RUNNING) {
-                timer.pause()
-                states = TimerStates.PAUSED
-            }
-        }
+        //Logs to test wifi
+//        Log.d("WifiInfo", "RSSI: $rssi dBm")
+//        Log.d("WifiName", "SSID: $ssid, Frequency: $frequency MHz, Link Speed: $linkSpeed")
 
-        resetButton.setOnClickListener {
-            if(states == TimerStates.RUNNING || states == TimerStates.PAUSED) {
-                rssiText.text = "0"
-                timer.reset()
-                states = TimerStates.CANCELLED
-            }
-        }
-    }
-}
 
-class HandlerTimer {
-    private val handler = Handler(Looper.getMainLooper())
-    private lateinit var runnable: Runnable
-    private var isPaused = false
-    private var time = 0
+        var wifiInfo: WifiInfo? = null
 
-    @SuppressLint("SetTextI18n")
-    fun start(text: TextView) {
+
+
         runnable = Runnable {
-            if(!isPaused) {
-                text.text = "$time"
-                time++
+
+            val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetwork = connectivityManager.activeNetwork
+            val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+
+            if (networkCapabilities != null && networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                wifiInfo = networkCapabilities.transportInfo as? WifiInfo
             }
+
+            val rssi: Int = wifiInfo?.rssi ?: Log.d("ERROR", "Can't find RSSI")  // in dBm
+            val frequency: Int = wifiInfo?.frequency ?: Log.d("ERROR", "Can't find Frequency")
+            val ssid = wifiInfo?.ssid ?: Log.d("ERROR", "Can't find SSID")
+            val linkSpeed: Int = wifiInfo?.linkSpeed ?: Log.d("ERROR", "Can't find Link Speed")
+
+            Log.d("WifiInfo", "RSSI: $rssi dBm")
+
+            //rssiText.text = "$rssi dBm"
             handler.postDelayed(runnable, 1000)
         }
-        handler.post(runnable )
-    }
+        handler.post(runnable)
 
-    fun pause() {
-        isPaused = true
-    }
 
-    fun resume() {
-        isPaused = false
-    }
 
-    fun reset() {
-        handler.removeCallbacks(runnable)
-        time = 0
-        isPaused = false
+//        var states: TimerStates = TimerStates.INITIAL
+//        val timer = HandlerTimer()
+//
+//        startButton.setOnClickListener {
+//            if (states == TimerStates.INITIAL || states == TimerStates.CANCELLED) {
+//                timer.start(rssiText)
+//                states = TimerStates.RUNNING
+//            } else if (states == TimerStates.PAUSED) {
+//                timer.resume()
+//                states = TimerStates.RUNNING
+//            }
+//        }
+//
+//        pauseButton.setOnClickListener {
+//            if (states == TimerStates.RUNNING) {
+//                timer.pause()
+//                states = TimerStates.PAUSED
+//            }
+//        }
+//
+//        resetButton.setOnClickListener {
+//            if (states == TimerStates.RUNNING || states == TimerStates.PAUSED) {
+//                rssiText.text = "0"
+//                timer.reset()
+//                states = TimerStates.CANCELLED
+//            }
+//        }
     }
-
 }
+
+//class HandlerTimer {
+//    private val handler = Handler(Looper.getMainLooper())
+//    private lateinit var runnable: Runnable
+//    private var isPaused = false
+//    private var time = 0
+//
+//    @SuppressLint("SetTextI18n")
+//    fun start(text: TextView) {
+//        runnable = Runnable {
+//            if(!isPaused) {
+//                text.text = "$time"
+//                time++
+//            }
+//            handler.postDelayed(runnable, 1000)
+//        }
+//        handler.post(runnable )
+//    }
+//
+//    fun pause() {
+//        isPaused = true
+//    }
+//
+//    fun resume() {
+//        isPaused = false
+//    }
+//
+//    fun reset() {
+//        handler.removeCallbacks(runnable)
+//        time = 0
+//        isPaused = false
+//    }
+//
+//}
