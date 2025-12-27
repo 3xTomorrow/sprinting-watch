6418d05854166202639e1666a869007f79aad395
3xTomorrow <denyszagrebin7@gmail.com>
2025-12-26 22:59:51 -0500

    Added title screen

--- java/com/example/sprintingwatch/presentation/MainActivity.kt
+++ java/com/example/sprintingwatch/presentation/MainActivity.kt
@@ -35,111 +35,118 @@ class MainActivity : ComponentActivity() {
 
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.main_layout)
+        setContentView(R.layout.title_screen)
 
         //Creates variables for the button
-        val startButton: Button = findViewById(R.id.start_button)
-        val resetButton: Button = findViewById(R.id.reset_button)
-        val pauseButton: Button = findViewById(R.id.pause_button)
+        val beginButton: Button = findViewById(R.id.begin_button)
 
-        //Creates variables for the texts
-        val reachGoalText: TextView = findViewById(R.id.reachedGoalText)
-        val rssiText: TextView = findViewById(R.id.rssiText)
-        val elapsedTimeText: TextView = findViewById(R.id.elapsedTimeText)
+        beginButton.setOnClickListener {
+            setContentView(R.layout.main_layout)
 
-        //Event Loop Variables
-        val handler = Handler(Looper.getMainLooper())
-        lateinit var runnable: Runnable
+            val startButton: Button = findViewById(R.id.start_button)
+            val resetButton: Button = findViewById(R.id.reset_button)
+            val pauseButton: Button = findViewById(R.id.pause_button)
 
-        //WiFi Variables
-        var wifiInfo: WifiInfo? = null
+            //Creates variables for the texts
+            val reachGoalText: TextView = findViewById(R.id.reachedGoalText)
+            val rssiText: TextView = findViewById(R.id.rssiText)
+            val elapsedTimeText: TextView = findViewById(R.id.elapsedTimeText)
 
-        //Other Variables
-        var gettingRSSI: Boolean = false
-        var reachedFinishLine: Boolean = false
-        var paused: Boolean = true
-        var elapsedTime: Double = 0.0
+            //Event Loop Variables
+            val handler = Handler(Looper.getMainLooper())
+            lateinit var runnable: Runnable
 
-        //Button Functions that change whether the program should be gathering RSSI or not.
-        startButton.setOnClickListener {
-            gettingRSSI = true
-            paused = false
-        }
-        pauseButton.setOnClickListener {
-            gettingRSSI = false
-            paused = true
-        }
-        resetButton.setOnClickListener {
-            if(paused) {
-                elapsedTime = 0.0
-                runOnUiThread { elapsedTimeText.text = "$elapsedTime" }
+            //WiFi Variables
+            var wifiInfo: WifiInfo? = null
+
+            //Other Variables
+            var gettingRSSI: Boolean = false
+            var reachedFinishLine: Boolean = false
+            var paused: Boolean = true
+            var elapsedTime: Double = 0.0
+
+            //Button Functions that change whether the program should be gathering RSSI or not.
+            startButton.setOnClickListener {
+                gettingRSSI = true
+                paused = false
             }
-        }
-
-
-
-
-        runnable = Runnable {
-            if (gettingRSSI) {
-
-                //Changes the time dislayed and formats it
-                elapsedTimeText.text = String.format("%.2f", elapsedTime)
-
-                //Uses connectivity manager API to gather the WiFi information and use for the RSSI
-                val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-                val activeNetwork = connectivityManager.activeNetwork
-                val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
-
-                if (networkCapabilities != null && networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-                    wifiInfo = networkCapabilities.transportInfo as? WifiInfo
+            pauseButton.setOnClickListener {
+                gettingRSSI = false
+                paused = true
+            }
+            resetButton.setOnClickListener {
+                if(paused) {
+                    elapsedTime = 0.0
+                    runOnUiThread { elapsedTimeText.text = "$elapsedTime" }
                 }
+            }
 
 
-                // WiFi info data
-                val rssi: Int = wifiInfo?.rssi ?: 10  // in dBm (10 for null value rather than -1 because -1 is a possible value of RSSI while 10 is not)
-                val frequency: Int = wifiInfo?.frequency ?: -1
-                val ssid = wifiInfo?.ssid ?: "Unknown"
-                val linkSpeed: Int = wifiInfo?.linkSpeed ?: -1
 
 
-                //Logs for RSSI if needed
+            runnable = Runnable {
+                if (gettingRSSI) {
+
+                    //Changes the time dislayed and formats it
+                    elapsedTimeText.text = String.format("%.2f", elapsedTime)
+
+                    //Uses connectivity manager API to gather the WiFi information and use for the RSSI
+                    val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+                    val activeNetwork = connectivityManager.activeNetwork
+                    val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+
+                    if (networkCapabilities != null && networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                        wifiInfo = networkCapabilities.transportInfo as? WifiInfo
+                    }
+
+
+                    // WiFi info data
+                    val rssi: Int = wifiInfo?.rssi ?: 10  // in dBm (10 for null value rather than -1 because -1 is a possible value of RSSI while 10 is not)
+                    val frequency: Int = wifiInfo?.frequency ?: -1
+                    val ssid = wifiInfo?.ssid ?: "Unknown"
+                    val linkSpeed: Int = wifiInfo?.linkSpeed ?: -1
+
+
+                    //Logs for RSSI if needed
 //                if(rssi != 10)
 //                    Log.d("WifiInfo", "RSSI: $rssi")
 //                else
 //                    Log.d("WifiInfo", "No Wi-Fi connection")
 
-                //Changes the display on the screen depending on if there is a connection
-                if(rssi != 10)
-                    rssiText.text = "$rssi dBm"
-                else
-                    rssiText.text = "No Wi-Fi signal"
+                    //Changes the display on the screen depending on if there is a connection
+                    if(rssi != 10)
+                        rssiText.text = "$rssi dBm"
+                    else
+                        rssiText.text = "No Wi-Fi signal"
 
-                //Variable for seeing if crossed finish line
-                reachedFinishLine = rssi > -20
+                    //Variable for seeing if crossed finish line
+                    reachedFinishLine = rssi > -20
 
-                //Conditional for crossing finish line
-                if(reachedFinishLine) {
-                    reachGoalText.text = "Yes"
-                    paused = true
+                    //Conditional for crossing finish line
+                    if(reachedFinishLine) {
+                        reachGoalText.text = "Yes"
+                        paused = true
+                    } else {
+                        reachGoalText.text = "No"
+                        paused = false
+                    }
+
                 } else {
-                    reachGoalText.text = "No"
-                    paused = false
+                    //Displayed when user isn't getting the RSSI
+                    rssiText.text = "Not getting RSSI"
                 }
 
-            } else {
-                //Displayed when user isn't getting the RSSI
-                rssiText.text = "Not getting RSSI"
-            }
+                //Increases the time if the user is not paused
+                if(!paused) {
+                    elapsedTime += .01
+                }
 
-            //Increases the time if the user is not paused
-            if(!paused) {
-                elapsedTime += .01
+                //rssiText.text = "$rssi dBm"
+                handler.postDelayed(runnable, 10)
             }
-
-            //rssiText.text = "$rssi dBm"
-            handler.postDelayed(runnable, 10)
+            handler.post(runnable)
         }
-        handler.post(runnable)
+
 
 
     }
