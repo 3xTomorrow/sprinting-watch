50bacd5cc50106c49e0a46173adfb6f21dc590bd
3xTomorrow <denyszagrebin7@gmail.com>
2026-01-02 15:29:28 -0500

    Created Bluetooth permission and bluetooth scan for devices, logged device name and address when found.

--- java/com/example/sprintingwatch/presentation/MainActivity.kt
+++ java/com/example/sprintingwatch/presentation/MainActivity.kt
@@ -23,13 +23,22 @@ import java.io.File
 import java.io.IOException
 import java.util.concurrent.TimeUnit
 import kotlin.arrayOf
-
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : ComponentActivity() {
 
 
     @SuppressLint("SetTextI18n", "MissingInflatedId", "ServiceCast")
-    @RequiresPermission(allOf= [Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_FINE_LOCATION])
+    @RequiresPermission(allOf= [Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT])
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
 
@@ -40,7 +49,38 @@ class MainActivity : ComponentActivity() {
         //Creates variables for the button
         val beginButton: Button = findViewById(R.id.begin_button)
 
+        val PERMISSION_REQUEST_CODE = 1
+
+        //Function that requests user for these permissions (Find devices, connect to devices, and location)
+        fun requestBluetoothPermissions() {
+            val permissions = arrayOf(
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST_CODE)
+        }
+
+        //Checks if the permissions have already been granted
+        fun hasBluetoothPermissions(): Boolean {
+            return run {
+                ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED
+            }
+        }
+
+        //Function that combines the the previous ones for permissions (Used before bluetooth is granted)
+        fun startBluetoothDiscovery() {
+            if(!hasBluetoothPermissions()) {
+                requestBluetoothPermissions()
+                return
+            }
+        }
+
         beginButton.setOnClickListener {
+            startBluetoothDiscovery()
+
             setContentView(R.layout.main_layout)
 
             val startButton: Button = findViewById(R.id.start_button)
@@ -69,10 +109,14 @@ class MainActivity : ComponentActivity() {
             startButton.setOnClickListener {
                 gettingRSSI = true
                 paused = false
+
+                Log.d("Bluetooth", BluetoothAdapter.ACTION_DISCOVERY_STARTED); //Logs that device started looking for bluetooth devices
             }
             pauseButton.setOnClickListener {
                 gettingRSSI = false
                 paused = true
+
+                Log.d("Bluetooth", BluetoothAdapter.ACTION_DISCOVERY_FINISHED) //Logs that device stopped looking for bluetooth devices
             }
             resetButton.setOnClickListener {
                 if(paused) {
@@ -81,7 +125,7 @@ class MainActivity : ComponentActivity() {
                 }
             }
 
-            // Bluetooth
+
 
 
             runnable = Runnable {
@@ -90,21 +134,22 @@ class MainActivity : ComponentActivity() {
                     //Changes the time displayed and formats it
                     elapsedTimeText.text = String.format("%.2f", elapsedTime)
 
-                    //Uses connectivity manager API to gather the WiFi information and use for the RSSI
-                    val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-                    val activeNetwork = connectivityManager.activeNetwork
-                    val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
 
-                    if (networkCapabilities != null && networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-                        wifiInfo = networkCapabilities.transportInfo as? WifiInfo
+                    //Creates the adapter on the watch
+                    val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+                    //Creates a filter for any Bluetooth devices that is found
+                    val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+                    //Runs the receiver function when the filter device is found
+                    registerReceiver(receiver, filter)
+
+                    //If the bluetooth adapter is enabled, start looking for other bluetooth devices
+                    if(bluetoothAdapter?.isEnabled == true) {
+                        bluetoothAdapter.startDiscovery()
                     }
 
 
-                    // WiFi info data
-                    val rssi: Int = wifiInfo?.rssi ?: 10  // in dBm (10 for null value rather than -1 because -1 is a possible value of RSSI while 10 is not)
-                    val frequency: Int = wifiInfo?.frequency ?: -1
-                    val ssid = wifiInfo?.ssid ?: "Unknown"
-                    val linkSpeed: Int = wifiInfo?.linkSpeed ?: -1
+                    val rssi: Int = 0
 
 
                     //Logs for RSSI if needed
@@ -142,13 +187,27 @@ class MainActivity : ComponentActivity() {
                 }
 
                 //rssiText.text = "$rssi dBm"
-                handler.postDelayed(runnable, 10)
+                handler.postDelayed(runnable, 1000)
             }
             handler.post(runnable)
         }
 
 
 
+    }
+
+    //Receiver function that is used when a bluetooth device is found
+    private val receiver = object : BroadcastReceiver() {
+        @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
+        override fun onReceive(context: Context, intent: Intent) {
+            val action = intent.action
+            if (BluetoothDevice.ACTION_FOUND == action) {
+                val device: BluetoothDevice? = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                val deviceName = device?.name
+                val deviceAddress = device?.address
+                Log.d("Bluetooth", "Found device: $deviceName - $deviceAddress")
+            }
+        }
     }
 
     fun rssiParse(cmdOutput: String): String {
